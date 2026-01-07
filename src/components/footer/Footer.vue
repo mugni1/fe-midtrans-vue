606299f40fa89d79052e5a81ea82768222ee4be5
mugni1 <asepabdulmugni22@gmail.com>
2026-01-07 17:42:11 +0700

    refactor: improve footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import { Separator } from '../ui/separator'
-  import Wafe from './wafe.vue'
+  import Wafe from './Wafe.vue'
+  import { data } from './data'
 </script>
 
 <template>
@@ -10,70 +11,150 @@
       <div>
         <img
           src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2F85ee9e2e-cbda-4e15-955e-1696e2ff9f27.png&w=640&q=100"
-          alt=""
+          alt="brand"
           class="w-4/12 lg:w-1/12"
         />
       </div>
       <div class="grid grid-cols-2 lg:grid-cols-6 gap-8">
         <div class="space-y-8 col-span-2 lg:col-span-3">
-          <p class="font-normal text-sm">
-            No #1 supplier top up game & voucher terlaris, murah, aman legal 100% buka 24
-            Jam dengan channel pembayaran terlengkap Indonesia.
-          </p>
+          <p class="font-normal text-sm">{{ data.description }}</p>
           <div class="flex gap-8">
-            <img
-              class="size-6"
-              src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2Ff6a1857c-6ced-41ab-99fb-2340b2dbe09c.webp&w=32&q=75"
-              alt=""
-            />
-            <img
-              class="size-6"
-              src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2F8f4f6f16-4763-4733-a595-c37c9aae94f1.webp&w=32&q=75"
-              alt=""
-            />
-            <img
-              class="size-6"
-              src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2F509573ad-c36d-467e-884a-72a984a95330.webp&w=32&q=75"
-              alt=""
-            />
-            <img
-              class="size-6"
-              src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2Ff233c760-d50d-40f6-a425-800559888c18.webp&w=32&q=75"
-              alt=""
-            />
+            <a v-if="data.facebook_url" :href="data.facebook_url" target="_blank">
+              <img
+                class="size-6"
+                src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2Ff6a1857c-6ced-41ab-99fb-2340b2dbe09c.webp&w=32&q=75"
+                alt=""
+              />
+            </a>
+            <a v-if="data.email_url" :href="data.email_url" target="_blank">
+              <img
+                class="size-6"
+                src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2F8f4f6f16-4763-4733-a595-c37c9aae94f1.webp&w=32&q=75"
+                alt=""
+              />
+            </a>
+            <a v-if="data.instagram_url" :href="data.instagram_url" target="_blank">
+              <img
+                class="size-6"
+                src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2F509573ad-c36d-467e-884a-72a984a95330.webp&w=32&q=75"
+                alt=""
+              />
+            </a>
+            <a v-if="data.telegram_url" :href="data.telegram_url" target="_blank">
+              <img
+                class="size-6"
+                src="https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2Ff233c760-d50d-40f6-a425-800559888c18.webp&w=32&q=75"
+                alt=""
+              />
+            </a>
           </div>
           <div class="space-y-1">
-            <p class="text-sm font-normal">Download BANGJEFF Mobile App on:</p>
-            <img
-              class="h-8"
-              src="https://www.bangjeff.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgoogle-play-android.4acd875c.png&w=128&q=75"
-              alt=""
-            />
+            <p class="text-sm font-normal">
+              Download {{ data.brand_name }} Mobile App on:
+            </p>
+            <div class="flex gap-2">
+              <a v-if="data.play_store_url" :href="data.play_store_url" target="_blank">
+                <img
+                  class="h-8"
+                  src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+                  alt="play_store"
+                />
+              </a>
+              <a v-if="data.app_store" :href="data.app_store" target="_blank">
+                <img
+                  class="h-8"
+                  src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg"
+                  alt="app_store"
+                />
+              </a>
+            </div>
           </div>
         </div>
         <div class="gap-4 flex flex-col col-span-1 lg:col-span-1">
           <b class="text-primary">Partnership</b>
           <ul class="font-normal space-y-2 text-sm">
-            <li>Reseller</li>
-            <li>Web Creation</li>
-            <li>Affiliate</li>
-            <li>Career</li>
+            <li>
+              <a
+                v-if="data.partnership.reseller"
+                :href="data.partnership.reseller"
+                target="_blank"
+                >Reseller</a
+              >
+            </li>
+            <li>
+              <a
+                v-if="data.partnership.web_creation"
+                :href="data.partnership.web_creation"
+                target="_blank"
+                >Web Creation</a
+              >
+            </li>
+            <li>
+              <a
+                v-if="data.partnership.affiliate"
+                :href="data.partnership.affiliate"
+                target="_blank"
+                >Affiliate</a
+              >
+            </li>
+            <li>
+              <a
+                v-if="data.partnership.career"
+                :href="data.partnership.career"
+                target="_blank"
+                >Career</a
+              >
+            </li>
           </ul>
         </div>
         <div class="gap-4 flex flex-col col-span-1 lg:col-span-1">
           <b class="text-primary">Site Map</b>
           <ul class="font-normal space-y-2 text-sm">
-            <li>Contac Us</li>
-            <li>Reviews</li>
-            <li>Terms & Conditions</li>
-            <li>Privacy Police</li>
+            <li>
+              <a
+                v-if="data.site_map.contac_us"
+                :href="data.site_map.contac_us"
+                target="_blank"
+                >Contac Us</a
+              >
+            </li>
+            <li>
+              <a
+                v-if="data.site_map.reviews"
+                :href="data.site_map.reviews"
+                target="_blank"
+                >Reviews</a
+              >
+            </li>
+            <li>
+              <a v-if="data.site_map.tnc" :href="data.site_map.tnc" target="_blank"
+                >Terms & Conditions</a
+              >
+            </li>
+            <li>
+              <a v-if="data.site_map.pp" :href="data.site_map.pp" target="_blank"
+                >Privacy Police</a
+              >
+            </li>
           </ul>
         </div>
         <div class="gap-4 flex flex-col col-span-1 lg:col-span-1">
           <b class="text-primary">Support</b>
           <ul class="font-normal space-y-2 text-sm">
-            <li>Telegram</li>
-            <li>Line</li>
+            <li>
+              <a
+                v-if="data.support.telegram"
+                :href="data.support.telegram"
+                target="_blank"
+              >
+                Telegram
+              </a>
+            </li>
+            <li>
+              <a v-if="data.support.line" :href="data.support.line" target="_blank">
+                Line
+              </a>
+            </li>
           </ul>
         </div>
       </div>
@@ -81,7 +162,7 @@
     <Separator class="bg-muted-foreground" />
     <section class="container mx-auto px-4 font-normal py-6">
       <p class="text-muted-foreground text-sm text-center md:text-start">
-        &copy; {{ new Date().getFullYear() }} BANGJEFF. All right reserved.
+        &copy; {{ new Date().getFullYear() }} {{ data.brand_name }}. All right reserved.
       </p>
     </section>
   </footer>
---
+++ src/components/footer/data.ts
@@ -0,0 +1,27 @@
+export const data = {
+  brand_name: "BANGJEFF",
+  brand_url: 'https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2F85ee9e2e-cbda-4e15-955e-1696e2ff9f27.png&w=640&q=100',
+  description: 'No #1 supplier top up game & voucher terlaris, murah, aman legal 100% buka 24 Jam dengan channel pembayaran terlengkap Indonesia.',
+  play_store_url: "https://play.google.com/store/apps/details?id=com.bangjeff.app&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1",
+  app_store: null,
+  facebook_url: "https://www.facebook.com/officialbangjeff",
+  instagram_url: "https://instagram.com/bangjeffcom",
+  telegram_url: "https://t.me/bangjeffcom",
+  email_url: "mailto:cs@bangjeff.com",
+  partnership: {
+    reseller: "https://reseller.bangjeff.com/",
+    web_creation: "https://www.bjkreatif.co.id/",
+    affiliate: "https://www.bangjeff.com/id-id/affiliate",
+    career: "mailto:partnership@bangjeff.com"
+  },
+  site_map: {
+    contac_us: "https://www.bangjeff.com/id-id/contact-us",
+    reviews: "https://www.bangjeff.com/id-id/reviews",
+    tnc: "https://www.bangjeff.com/id-id/terms-and-condition",
+    pp: "https://www.bangjeff.com/id-id/privacy-policy"
+  },
+  support: {
+    telegram: "https://t.me/bangjeffcom",
+    line: "https://lin.ee/HNtkpFk"
+  }
+}
