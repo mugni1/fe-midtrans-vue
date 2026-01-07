a9e38e7360199db0b8bb84dc5619f768aaf2ba6c
mugni1 <asepabdulmugni22@gmail.com>
2026-01-07 09:57:42 +0700

    refactor: improve navbar

--- src/components/navbar/NavbarMenuPhone.vue
+++ src/components/navbar/NavbarMenuPhone.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
   import { motion } from 'motion-v'
   import { Button } from '../ui/button'
-  import { ShoppingBag } from 'lucide-vue-next'
+  import { CalendarClockIcon, Scale, ShoppingBag } from 'lucide-vue-next'
 
   const isOpen = defineModel<boolean>()
 </script>
 
 <template>
-  <section
-    class="fixed inset-0 w-full z-40 px-4 lg:hidden transition-all duration-200"
-    :class="isOpen && 'backdrop-blur-sm'"
+  <motion.section
+    :class="
+      isOpen
+        ? 'fixed inset-0 z-40 px-4 lg:hidden backdrop-blur-sm transition-all duration-200'
+        : 'invisible fixed inset-0 z-40 px-4 lg:hidden transition-all duration-200'
+    "
   >
     <motion.div
       class="container mx-auto bg-card border rounded-md mt-24 p-4 space-y-4"
@@ -18,8 +21,8 @@
       :transition="{ duration: 0.2 }"
     >
       <Button variant="outline" class="w-full"><ShoppingBag /> Top Up</Button>
-      <Button variant="outline" class="w-full"><ShoppingBag /> Top Up</Button>
-      <Button variant="outline" class="w-full"><ShoppingBag /> Top Up</Button>
+      <Button variant="outline" class="w-full"><CalendarClockIcon /> Transaction </Button>
+      <Button variant="outline" class="w-full"><Scale /> Calculator </Button>
     </motion.div>
-  </section>
+  </motion.section>
 </template>
